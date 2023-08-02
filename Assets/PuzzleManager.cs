7613ab92f07ee7c908df5358041cc76a7fdfc8da
rampure4 <rampure@wisc.edu>
2023-08-02 11:00:07 -0500

    puzzle manger

---
+++ Assets/PuzzleManager.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public GameObject[] puzzlePieceHolder1;
+    public GameObject[] puzzlePieceHolder2;
+    public GameObject[] puzzlePieceHolder3;
+
+
+    private int purpleCounter;
+    private int coralCounter;
+    private int blueCounter;
+
+    public void purpleATM()
+    {
+
+        purpleCounter++;
+        switch (purpleCounter)
+        {
+            case 0:
+                puzzlePieceHolder1[0].SetActive(true);
+                puzzlePieceHolder1[1].SetActive(false);
+                puzzlePieceHolder1[2].SetActive(false);
+                break;
+            case 1:
+                puzzlePieceHolder1[0].SetActive(false);
+                puzzlePieceHolder1[1].SetActive(true);
+                puzzlePieceHolder1[2].SetActive(false);
+                break;
+            case 2:
+                puzzlePieceHolder1[0].SetActive(false);
+                puzzlePieceHolder1[1].SetActive(false);
+                puzzlePieceHolder1[2].SetActive(true);
+                break;
+            case 3:
+                puzzlePieceHolder1[0].SetActive(true);
+                puzzlePieceHolder1[1].SetActive(false);
+                puzzlePieceHolder1[2].SetActive(false);
+                purpleCounter = 0;
+                break;
+        }
+
+
+
+
+    }
+    public void coralATM()
+    {
+
+        coralCounter++;
+        switch (coralCounter)
+        {
+            case 0:
+                puzzlePieceHolder2[0].SetActive(true);
+                puzzlePieceHolder2[1].SetActive(false);
+                puzzlePieceHolder2[2].SetActive(false);
+                break;
+            case 1:
+                puzzlePieceHolder2[0].SetActive(false);
+                puzzlePieceHolder2[1].SetActive(true);
+                puzzlePieceHolder2[2].SetActive(false);
+                break;
+            case 2:
+                puzzlePieceHolder2[0].SetActive(false);
+                puzzlePieceHolder2[1].SetActive(false);
+                puzzlePieceHolder2[2].SetActive(true);
+                break;
+            case 3:
+                puzzlePieceHolder2[0].SetActive(true);
+                puzzlePieceHolder2[1].SetActive(false);
+                puzzlePieceHolder2[2].SetActive(false);
+                coralCounter = 0;
+                break;
+        }
+
+
+
+
+    }
+    public void blueATM()
+    {
+
+        blueCounter++;
+        switch (blueCounter)
+        {
+            case 0:
+                puzzlePieceHolder3[0].SetActive(true);
+                puzzlePieceHolder3[1].SetActive(false);
+                puzzlePieceHolder3[2].SetActive(false);
+                break;
+            case 1:
+                puzzlePieceHolder3[0].SetActive(false);
+                puzzlePieceHolder3[1].SetActive(true);
+                puzzlePieceHolder3[2].SetActive(false);
+                break;
+            case 2:
+                puzzlePieceHolder3[0].SetActive(false);
+                puzzlePieceHolder3[1].SetActive(false);
+                puzzlePieceHolder3[2].SetActive(true);
+                break;
+            case 3:
+                puzzlePieceHolder3[0].SetActive(true);
+                puzzlePieceHolder3[1].SetActive(false);
+                puzzlePieceHolder3[2].SetActive(false);
+                blueCounter = 0;
+                break;
+        }
+
+
+
+
+    }
+}
